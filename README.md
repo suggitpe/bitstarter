679ac6b4dc76cd8a69ef0a3d66baa7ca8f326bb6
Peter Suggitt <me@suggs.org.uk>
2013-07-08 19:30:08 +0100

    Updated to read from index.html

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 ## Description
 This is a Javascript repo for a coursera startup engineering course.  This is a forked and then extended heroku website.
+
+## Reminder of how to use
+
+### Run locally
+node web.js
+
+### Run on Heroku
+git push heroku master
--- web.js
+++ web.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require('express')
+var fs = require('fs')
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger())
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+	fs.readFile('index.html', 'utf-8', function(err, data){
+		if(err) throw err
+		response.send(data);
+	})  
+})
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+  console.log("Listening on " + port)
+})
